83d800bf536870bda1b22c92cf7355c1e64be58d
James McMath <mcmathja@gmail.com>
2015-11-09 14:16:00 -0600

    Remove unimplemented payloads

--- src/main/scala/io/chapbook/util/auth/jwt/Payload.scala
+++ src/main/scala/io/chapbook/util/auth/jwt/Payload.scala
@@ -10,33 +10,13 @@ import java.util.UUID
 abstract class Payload
 
 object Claims {
-  trait Aud extends Payload {
-    val aud: Seq[String]
-  }
-
   trait Exp extends Payload {
     val exp: Long
     if(exp < System.currentTimeMillis) throw new SecurityException("Token expired.")
   }
 
-  trait Jti extends Payload {
-    val jti: UUID = UUID.randomUUID
-  }
-
-  trait Iat extends Payload {
-    val iat: Long = System.currentTimeMillis
-  }
-
-  trait Iss extends Payload {
-    val iss: String
-  }
-
   trait Nbf extends Payload {
     val nbf: Long
     if(nbf > System.currentTimeMillis) throw new SecurityException("Token not yet valid.")
-  }
-
-  trait Sub extends Payload {
-    val sub: String
   }
 }
